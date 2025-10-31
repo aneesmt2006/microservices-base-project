8fd702fb15e96d9a2a2b6d75a181c9271d5e623a
aneesmt2006 <aneesanu2006@gmail.com>
2025-10-31 14:14:39 +0530

    Independent base project template

---
+++ src/config/db.config.ts
@@ -0,0 +1,19 @@
+// import mongoose from "mongoose";
+// import { config } from "./env.config.js";
+
+// export const connectDB = async () => {
+// //   const url = config.mongoUrl;
+//   try {
+//     // if (!url) {
+//       throw new Error("DB url is Missing");
+//     }
+
+//     await mongoose.connect(url, {
+//       dbName: "Your-service name",
+//     });
+//     console.log("Connecting to MongoDB success");
+//   } catch (error) {
+//     console.error("Failed to connect DB", error);
+//     process.exit(1);
+//   }
+// };
